9bc35d88d3afe9d3c23cf596c9949115ce1a4b6e
Shahnaz538 <131960221+Shahnaz538@users.noreply.github.com>
2023-10-05 16:35:21 +0500

    Day 2 Practice on Variables and Data Types

---
+++ Basic/01_variables.js
@@ -0,0 +1,23 @@
+
+// Variable redeclaration 
+//const variable: 
+const accountId = 17278;
+//let variable:
+let email = "SHahnaz2y938@gmail.com";
+//var variable:
+ var password = "198";
+ city="Larkana";
+
+ //can't redecalre accountId (const) variabe
+//  accountId =78;
+ email="mariamemon2776@gmail.com";
+ password="278826186eee"
+ city="Dadu";
+ let state;
+//  console.log(accountId);
+//  Another way to use console withconsole.table
+
+ console.table([accountId, email, city, password, state]);
+//  var has scope problem so that we use let
+/* Prefer not to use var
+bcs of issue in block scope and functional scope */
---
+++ Basic/02_DataTypes.js
@@ -0,0 +1,21 @@
+"use strict";//Treat all JS codes as newer version
+//can't use is node  only in brower
+// alert("hello")
+console.log("74387sdfjk"); console.log(28); //code readibilty should be high
+// Primitive Data Types
+// 1 number => range is 2 to power 53
+let age=22;
+// 2 bigint for very large number
+// 3 string ""/ ''
+let name="Shahnaz";
+// 4 Boolean => true/false
+let loggedin = false;
+// 5 null => Standalone value "Value is not any valu because 0 is number"
+let state = null; //object
+
+// 6 undefined => variable defined not assigned
+let temperature; //undefined 
+
+//7 Symbol => 
+//Non Primitive Data Types
+console.log(typeof temperature);
